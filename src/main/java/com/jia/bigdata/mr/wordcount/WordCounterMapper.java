9dfcc33c7e24f3baf9d4bb26a7933a7898074f13
crownprince <378097217@qq.com>
2019-07-25 01:20:03 +0800

    mapper

--- src/main/java/com/jia/bigdata/mr/wordcount/WordCounterMapper.java
+++ src/main/java/com/jia/bigdata/mr/wordcount/WordCounterMapper.java
@@ -7,17 +7,16 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
+
 /**
  * @author tanjia
  * @email 378097217@qq.com
  * @date 2019/7/15 2:09
  */
-public class WordCounterMapper extends Mapper<LongWritable, Text, Text, IntWritable>
-{
-    protected void map(final LongWritable key, final Text value, final Mapper.Context context) throws IOException, InterruptedException {
-        Arrays.stream(value.toString().split("\\s+")).filter(Objects::nonNull).filter(StringUtils::isNotBlank).forEach(e -> ExceptionWrapper.run(() -> context.write((Object)new Text(e), (Object)new IntWritable(1))));
+public class WordCounterMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    protected void map(final LongWritable key, final Text value, final Mapper.Context context) {
+        Arrays.stream(value.toString().split("\\s+")).filter(Objects::nonNull).filter(StringUtils::isNotBlank).forEach(e -> ExceptionWrapper.run(() -> context.write(new Text(e), new IntWritable(1))));
     }
 }
